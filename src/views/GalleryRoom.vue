f1751e425bb858c06eb2ef37ca5c92c83ae24dfa
Peerapat Padtawaro <peerapat.p@houseofdev.tech>
2026-03-12 16:43:58 +0700

    3D Carousel

--- src/views/GalleryRoom.vue
+++ src/views/GalleryRoom.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="min-h-screen flex flex-col bg-image overflow-hidden">
     <GalleryRoomCarousel class="relative" />
-    <CloseIcon class="absolute bottom-[7%] left-1/2 transform -translate-x-1/2 w-[7vh] cursor-pointer" />
+    <!-- <CloseIcon class="absolute bottom-[7%] left-1/2 transform -translate-x-1/2 w-[7vh] cursor-pointer" /> -->
   </div>
 </template>
 
